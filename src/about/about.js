2813977d0c82ab9aa366f449542cc2e57a974ece
AJ Olson <silentx.alex@gmail.com>
2020-08-11 16:22:22 -0400

    added about content

---
+++ src/about/about.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <section className="about">
+                <p className="missionStatement">
+                Web Developer skilled in Javascript and React. 
+                Proven track record in building strong client relations. 
+                Driven by collaborative environments that allow me to flex my creative problem skills coupled with a knack for critical thinking.
+                </p>
+                <p className="desc">
+                    I am a Full Stack Web Developer living outside the Ann Arbor, MI area.
+                    Developing, designing, implenting is my new passion in life where I can strive to create fun, interactive, engaging, and intuitive user experiences.
+                    You can catch me munching on deep dish pizza (Chicago Style), playing video games or expanding on my knowledge.
+                </p>
+            </section>
+        )
+    }
+}
+
+export default About;
