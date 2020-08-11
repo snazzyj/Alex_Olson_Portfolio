585c374bf31b0b4f5c814da8201ceb94959bca88
AJ Olson <silentx.alex@gmail.com>
2020-08-11 16:23:15 -0400

    updated skills and exporting of dev icons

---
+++ src/skills/skillIcons.js
@@ -0,0 +1,70 @@
+import Javascript from '../assests/icons/javascript_icon_130900.png';
+import React from '../assests/icons/react_original_logo_icon_146374.png';
+import jQuery from '../assests/icons/jquery_original_wordmark_logo_icon_146447.png';
+import HTML5 from '../assests/icons/html_original_wordmark_logo_icon_146478.png';
+import CSS3 from '../assests/icons/css_original_wordmark_logo_icon_146576.png';
+import Node from '../assests/icons/nodejs_original_wordmark_logo_icon_146412.png';
+import PostgreSQL from '../assests/icons/postgresql_plain_wordmark_logo_icon_146390.png';
+import Express from '../assests/icons/express_original_wordmark_logo_icon_146528.png'
+import NPM from '../assests/icons/npm_original_wordmark_logo_icon_146402.png';
+import Heroku from '../assests/icons/heroku_original_wordmark_logo_icon_146482.png';
+import GitHub from '../assests/icons/github-logo_icon-icons.com_73546.png'
+
+const frontend = [
+    {
+        alt: 'Javascript',
+        icon: Javascript,
+    },
+    {
+        alt: 'React',
+        icon: React,
+    },
+    {
+        alt: 'jQuery',
+        icon: jQuery,
+    },
+    {
+        alt: 'HTML5',
+        icon: HTML5,
+    },
+    {
+        alt: 'CSS3',
+        icon: CSS3,
+    }
+]
+
+const backend = [
+    {
+        alt: 'Node',
+        icon: Node
+    },
+    {
+        alt: 'PostgreSQL',
+        icon: PostgreSQL
+    },
+    {
+        alt: 'ExpressJs',
+        icon: Express
+    }
+]
+
+const other = [
+    {
+        alt: 'NPM',
+        icon: NPM
+    },
+    {
+        alt: 'Heroku',
+        icon: Heroku
+    },
+    {
+        alt: 'Github',
+        icon: GitHub
+    }
+]
+
+export {
+    frontend,
+    backend,
+    other
+};
---
+++ src/skills/skills.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {frontend, backend, other} from './skillIcons';
+import './skills.css'
+
+class Skills extends Component {
+    render() {
+        return (
+            <section className="skills">
+                <div className="frontend">
+                    {frontend.map((icon, index) => {
+                        return (
+                            <div key={index}>
+                                <img src={icon.icon} alt={icon.alt} className="icons"/>
+                                <p>{icon.alt}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+
+                <div className="backend">
+                    {backend.map((icon, index) => {
+                        return (
+                            <div key={index}>
+                                <img src={icon.icon} alt={icon.alt} className="icons" />
+                                <p>{icon.alt}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+
+                <div className="other">
+                    {other.map((icon, index) => {
+                        return (
+                            <div key={index}>
+                                <img src={icon.icon} alt={icon.alt} className="icons" />
+                                <p>{icon.alt}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Skills
